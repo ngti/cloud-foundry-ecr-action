269fb629325b9f167909181e4c76b1bac668704d
Guilherme Borges <guilherme@ngti.nl>
2019-11-04 11:51:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cf login -a $CF_API_ENDPOINT -o $ORG -s $SPACE -u $USERNAME -p $PASSWORD
 
 if [ -z "$DOCKER_IMAGE" ]
 then
-  CF_DOCKER_PASSWORD=$DOCKER_HUB_PASSWORD cf push $APP_NAME --docker-image $DOCKER_IMAGE --docker-username $DOCKER_HUB_USERNAME
+  CF_DOCKER_PASSWORD=$CF_DOCKER_PASSWORD cf push $APP_NAME --docker-image $CF_DOCKER_IMAGE --docker-username $CF_DOCKER_USERNAME
 else
   cf push $APP_NAME -p $GITHUB_WORKSPACE/$ARTIFACT_PATH
 fi
