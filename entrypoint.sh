7f1b5dafab9a2adde5e5f12136e55dcbdce1ad79
Guilherme Borges <guilherme@ngti.nl>
2019-11-04 11:43:27 +0100

    Added validation to when there is a docker deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 
 cf login -a $CF_API_ENDPOINT -o $ORG -s $SPACE -u $USERNAME -p $PASSWORD
 
-cf push $APP_NAME -p $GITHUB_WORKSPACE/$ARTIFACT_PATH
+if [ -z "$DOCKER_IMAGE" ]
+then
+  CF_DOCKER_PASSWORD=$DOCKER_HUB_PASSWORD cf push $APP_NAME --docker-image $DOCKER_IMAGE --docker-username $DOCKER_HUB_USERNAME
+else
+  cf push $APP_NAME -p $GITHUB_WORKSPACE/$ARTIFACT_PATH
+fi
