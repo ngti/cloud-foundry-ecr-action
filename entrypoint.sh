bc756fe4f160ff8eb0d6d12166a17b1fb3c9bd6a
Guilherme Borges <guilherme@ngti.nl>
2019-11-05 10:59:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 cf login -a $CF_API_ENDPOINT -o $ORG -s $SPACE -u $USERNAME -p $PASSWORD
 
-if [[ ${DOCKER_IMAGE} ]];
+if [[ ${CF_DOCKER_IMAGE} ]];
 then
   CF_DOCKER_PASSWORD=$CF_DOCKER_PASSWORD cf push $APP_NAME --docker-image $CF_DOCKER_IMAGE --docker-username $CF_DOCKER_USERNAME
 else
