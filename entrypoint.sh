afa1f1a7adc1887e3c7a23699114da103044386a
Volkodav Anatolii, DBU-ION (EXT) <Anatolii.Volkodav@swisscom.com>
2020-05-08 11:53:03 +0200

    added support of manifest configuration file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,40 @@
 
 set -e
 
-export NUM_INSTANCES=${NUM_INSTANCES:-"1"}
-export DISK=${DISK:-"1G"}
-export MEMORY=${MEMORY:-"1G"}
-export HEALTH_CHECK_TYPE=${HEALTH_CHECK_TYPE:-"port"}
+die() {
+  echo >&2 "$@"
+  exit 1
+}
 
-export ARGS="-i $NUM_INSTANCES -k $DISK -m $MEMORY --health-check-type $HEALTH_CHECK_TYPE"
+[ -z $APP_NAME ] || die "APP_NAME argument required"
+[ -z $CF_API_ENDPOINT ] || die "CF_API_ENDPOINT argument required"
+[ -z $ORG ] || die "ORG argument required"
+[ -z $SPACE ] || die "SPACE argument required"
+[ -z $USERNAME ] || die "USERNAME argument required"
+[ -z $PASSWORD ] || die "PASSWORD argument required"
 
+# TODO: for debugging purpose only, will be removed shortly
+find .
+
+echo "Login to \"$CF_API_ENDPOINT\" using organization \"$ORG\" and space \"$SPACE\""
 cf login -a $CF_API_ENDPOINT -o $ORG -s $SPACE -u $USERNAME -p $PASSWORD
 
-if [[ ${CF_DOCKER_IMAGE} ]]; then
-  cf push $APP_NAME --docker-image $CF_DOCKER_IMAGE --docker-username $CF_DOCKER_USERNAME $ARGS
+if [[ ${CF_MANIFEST_FILE} ]]; then
+  echo "Deploy \"$APP_NAME\" using manifest file \"$CF_MANIFEST_FILE\""
+  cf push $APP_NAME -f $CF_MANIFEST_FILE
 else
-  cf push $APP_NAME -p $GITHUB_WORKSPACE/$ARTIFACT_PATH $ARGS
+  export NUM_INSTANCES=${NUM_INSTANCES:-"1"}
+  export DISK=${DISK:-"1G"}
+  export MEMORY=${MEMORY:-"1G"}
+  export HEALTH_CHECK_TYPE=${HEALTH_CHECK_TYPE:-"port"}
+
+  export ARGS="-i $NUM_INSTANCES -k $DISK -m $MEMORY --health-check-type $HEALTH_CHECK_TYPE"
+
+  if [[ ${CF_DOCKER_IMAGE} ]]; then
+    echo "Deploy \"$APP_NAME\" using docker image \"$CF_DOCKER_IMAGE\""
+    cf push $APP_NAME --docker-image $CF_DOCKER_IMAGE --docker-username $CF_DOCKER_USERNAME $ARGS
+  else
+    echo "Deploy \"$APP_NAME\" using app directory \"$GITHUB_WORKSPACE/$ARTIFACT_PATH\""
+    cf push $APP_NAME -p $GITHUB_WORKSPACE/$ARTIFACT_PATH $ARGS
+  fi
 fi
